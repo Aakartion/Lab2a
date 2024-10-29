b04e50bf88d352eff1b80fcff2a0821b0580bebf
aakarshan <aakartionsimkhada@gmail.com>
2024-10-29 14:38:06 -0500

    Employee and PensionPlan object is created

---
+++ src/main/java/edu/miu/cse/PensionPlan.java
@@ -0,0 +1,40 @@
+package edu.miu.cse;
+
+import java.time.LocalDate;
+
+public class PensionPlan {
+
+    private int planReferenceNumber;
+    private LocalDate enrollmentDate;
+    private double monthlyContribution;
+
+    public PensionPlan(int planReferenceNumber, LocalDate enrollmentDate, double monthlyContribution) {
+        this.planReferenceNumber = planReferenceNumber;
+        this.enrollmentDate = enrollmentDate;
+        this.monthlyContribution = monthlyContribution;
+    }
+
+    public int getPlanReferenceNumber() {
+        return planReferenceNumber;
+    }
+
+    public void setPlanReferenceNumber(int planReferenceNumber) {
+        this.planReferenceNumber = planReferenceNumber;
+    }
+
+    public LocalDate getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(LocalDate enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public double getMonthlyContribution() {
+        return monthlyContribution;
+    }
+
+    public void setMonthlyContribution(double monthlyContribution) {
+        this.monthlyContribution = monthlyContribution;
+    }
+}
